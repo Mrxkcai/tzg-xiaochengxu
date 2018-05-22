7dbe5b15c4011665522996aacd9d393b1819c2d3
zhuyl <zhuyl@cnjiang.com>
2018-05-22 11:28:04 +0800

    修改了bug

--- pages/login/login.js
+++ pages/login/login.js
@@ -35,29 +35,27 @@ Page({
   // 当验证码失去焦点时验证
   inputYan: function (e) {
     this.setData({
-              // show: !this.data.show,
-        sumbitShow: !this.data.sumbiShow
-     })
-    this.setData({
+      sumbitShow: !this.data.sumbiShow,
       inputYan: e.detail.value
     })
+    
   },
   //提交按钮的函数
   loginBtn: function () {
-    console.log(1)
-    // this.setData({
-    //     show: !this.data.show,
-    // })
     var that = this
-    if (this.data.inputPhone.length == 0||this.data.inputYan.length==0) {
+    if (this.data.inputPhone.length == 0&&this.data.inputYan.length==0) {
+      wx.showToast({
+        title: '手机号或验证码不能为空！',
+        icon: "none",
+        duration: 2000
+      })
+    } else if (this.data.inputPhone.length == 0 ||this.data.inputYan.length == 0){
       wx.showToast({
         title: '手机号或验证码错误！',
         icon: "none",
         duration: 2000
       })
-      
     } else {
-      console.log(this.data.inputYan)
       wx.request({
         url: 'https://api.taozugong.com/award/sms/verifyMobile',
         method: 'POST',
